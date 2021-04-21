64a4badfcaa9a2b82f8ae294fc4cd7c55d111f06
JungHyun <leahincom@gmail.com>
2021-04-22 00:08:00 +0900

    step5 done

--- assignment/2nd/js/clock.js
+++ assignment/2nd/js/clock.js
@@ -12,7 +12,6 @@ const monthArr = [
   "November",
   "December"
 ];
-let date, month, year, day, hour, min, sec, ampm; // 현재 시간 정보를 가지고 있는 객체입니다
 
 // 00 : 00의 형식을 맞추기 위해 한자리 수는 앞에 0을 채워주는 함수입니다
 const fillZero = (num) => {
@@ -25,9 +24,28 @@ const fillZero = (num) => {
   }
 };
 
+// 현재 시간 정보를 가지고 있는 객체입니다
+let date, year, month, day, hour, min, sec, ampm;
+
 const analogHour = document.querySelector(".analog_hour");
 const analogMin = document.querySelector(".analog_minute");
 const analogSec = document.querySelector(".analog_second");
+
+const digitalTime = document.querySelector(".digital_time");
+const digitalButton = document.querySelector(".digital_button");
+
+// initialization
+digitalButton.innerHTML = "24H";
+let hour24 = false; // 24시간인지 12시간인지 저장하는 전역변수를 지정합니다
+
+const changeTime = () => {
+  if (hour24) {
+    digitalButton.innerHTML = "24H"; // 버튼 내부의 텍스트도 바꿉니다
+    hour24 = false;
+  } else {
+    hour24 = true;
+  }
+};
 
 const synchronize = () => {
   date = new Date();
@@ -35,8 +53,14 @@ const synchronize = () => {
   year = date.getFullYear();
   day = fillZero(date.getDate());
   hour = date.getHours();
-  min = date.getMinutes();
-  sec = date.getSeconds();
+  // ampm
+  if (hour24) {
+    ampm = hour < 12 ? "AM" : "PM";
+    hour = hour === 0 ? 12 : hour > 12 ? hour - 12 : hour;
+    digitalButton.innerHTML = `${ampm}`;
+  }
+  min = fillZero(date.getMinutes());
+  sec = fillZero(date.getSeconds());
 
   // 시침의 각도를 계산합니다. 시침은 한 시간당 30도씩, 분당 0.5도씩 움직입니다
   // 90도를 빼는 이유는 바늘이 90도로 누워있는 상태에서 시작하기 때문입니다 (css 참고)
@@ -45,24 +69,21 @@ const synchronize = () => {
   const minDegree = min * (360 / 60) - 90;
   const secDegree = sec * (360 / 60) - 90;
 
+  hour = fillZero(hour);
+
   // css transform 속성 중 rotate를 다음과 같이 자바스크립트에서 사용할 수 있습니다
   analogHour.style.transform = `rotate(${hourDegree}deg)`;
   analogMin.style.transform = `rotate(${minDegree}deg)`;
   analogSec.style.transform = `rotate(${secDegree}deg)`;
 
-  ampm = hour < 12 ? "AM" : "PM";
-  hour = hour === 0 ? 12 : hour > 12 ? fillZero(hour - 12) : fillZero(hour);
-  min = fillZero(min);
-  sec = fillZero(sec);
-
   document.querySelector(
     ".main"
   ).innerHTML = `Today is <span class="red" style="color: red">${day} ${month}</span>, ${year}`; // 해당 클래스이름을 가진 요소를 가져옵니다
 
-  document.querySelector(
-    ".digital"
-  ).innerHTML = `${hour} : ${min} : ${sec} <span class="ampm" style="border-style: solid">${ampm}</span>`;
+  digitalTime.innerHTML = `${hour} : ${min} : ${sec}`;
+
+  // 버튼을 클릭하면 changeTime 함수를 호출하는 이벤트를 추가합니다
+  digitalButton.addEventListener("click", changeTime);
 };
 
 setInterval(synchronize, 1000); // 1초 간격으로 콜백함수를 실행합니다
-// drawArc();
