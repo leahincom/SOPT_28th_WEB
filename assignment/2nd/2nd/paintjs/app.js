477aa1c1bad35b4b0548ba740af81ed4ab6f6bd3
JungHyun <leahincom@gmail.com>
2021-04-23 00:53:30 +0900

    fill the canvas, fixing a bug of the vid (can draw while print mode), try different way to fill the canvas (not using fillRect but backgroundColor in event style)

--- assignment/2nd/2nd/paintjs/app.js
+++ assignment/2nd/2nd/paintjs/app.js
@@ -2,14 +2,22 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
 const palette = document.getElementsByClassName("controls__color");
+const range = document.getElementById("jsRange");
+const modeBtn = document.getElementById("jsMode");
 
-// all the feature in this context will use this value...
+const INITIAL_COLOR = "3c3c3c";
+
+// all the features in this context will use these values...
+// setting the default value
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
 
 let painting;
+let filling; // false: paint, true: fill
 
 function startPainting() {
-  painting = true;
+  painting = !filling ? true : false;
 }
 
 function stopPainting() {
@@ -41,8 +49,31 @@ function onMouseMove(event) {
 }
 
 function handleColorClick(event) {
-  // event.target.style.background
-  ctx.strokeStyle = event.target.style.backgroundColor;
+  let setColor = event.target.style.backgroundColor;
+  ctx.strokeStyle = setColor;
+  ctx.fillStyle = setColor;
+}
+
+function handleRangeChange(event) {
+  ctx.lineWidth = event.target.value;
+}
+
+function handleModeClick(event) {
+  // now fill -> change into print mode
+  if (filling) {
+    event.target.innerText = "fill";
+  }
+  // now print -> change into fill mode
+  else {
+    event.target.innerText = "print";
+  }
+  filling = !filling;
+}
+
+function fillCanvas(event) {
+  if (filling) {
+    event.target.style.backgroundColor = ctx.fillStyle;
+  }
 }
 
 // do this only if the canvas exists
@@ -51,7 +82,19 @@ if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", fillCanvas);
+}
+
+if (palette) {
   Array.from(palette).forEach((color) =>
     color.addEventListener("click", handleColorClick)
   );
+}
+
+if (range) {
+  range.addEventListener("input", handleRangeChange);
+}
+
+if (modeBtn) {
+  modeBtn.addEventListener("click", handleModeClick);
 }
