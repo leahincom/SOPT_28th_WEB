0b62a90b89fea8c1f38c86b7cb9e06beb3aa9511
JungHyun <leahincom@gmail.com>
2021-04-23 14:03:57 +0900

    step6 done

--- assignment/2nd/1st/js/weather.js
+++ assignment/2nd/1st/js/weather.js
@@ -1,5 +1,15 @@
 // require("dotenv").config();
 
+const weatherCanvas = document.getElementById("icon");
+
+// on Android, a nasty hack is needed: {"resizeClear": true}
+const skycons = new Skycons({
+  color: "white",
+  resizeClear: true
+});
+
+let icon;
+
 const key = "62371b7b36e39f14faad55d8c260981b";
 
 function handleSuccess(position) {
@@ -14,7 +24,7 @@ function handleError(err) {
 
 navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
 
-const weatherIcon = document.querySelector(".weather_icon");
+// const weatherIcon = document.querySelector(".weather_icon");
 const weatherTemp = document.querySelector(".weather_temp");
 const weatherShort = document.querySelector(".weather_short");
 const weatherDetail = document.querySelector(".weather_detail");
@@ -49,8 +59,58 @@ const getWeatherData = async (lat, lon) => {
   <span>Wind: </span>${weather.wind} m/s
   `;
 
-  // 다음 주소에 방금 가져온 아이콘 번호를 넣으면 아이콘 이미지를 가져올 수 있습니다
-  weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather.icon}@2x.png" style="width: 100%" alt="icon" />`;
+  /********************************* 아이콘 종류 ******************************
+   * CLEAR_DAY, CLEAR_NIGHT, PARTLY_CLOUDY_DAY, PARTLY_CLOUDY_NIGHT, CLOUDY,
+   * RAIN, SLEET, SNOW, WIND, FOG 총 10가지
+   ****************************************************************************/
+  switch (weather.icon) {
+    case "01d":
+      icon = Skycons.CLEAR_DAY;
+      break;
+    case "02d":
+      icon = Skycons.PARTLY_CLOUDY_DAY;
+      break;
+    case "03d":
+      icon = Skycons.CLOUDY;
+      break;
+    case "10d":
+      icon = Skycons.RAIN;
+      break;
+    case "13d":
+      icon = Skycons.SNOW;
+      break;
+    case "50d":
+      icon = Skycons.FOG;
+      break;
+    case "01n":
+      icon = Skycons.CLEAR_NIGHT;
+      break;
+    case "02n":
+      icon = Skycons.PARTLY_CLOUDY_NIGHT;
+      break;
+    case "03n":
+      icon = Skycons.CLOUDY;
+      break;
+    case "10n":
+      icon = Skycons.RAIN;
+      break;
+    case "13n":
+      icon = Skycons.SNOW;
+      break;
+    case "50n":
+      icon = Skycons.FOG;
+      break;
+  }
+
+  // you can add a canvas by it's ID...
+  // ...or by the canvas DOM element itself.
+  skycons.add(weatherCanvas, icon);
+
+  // start animation!
+  skycons.play();
+
+  // // 다음 주소에 방금 가져온 아이콘 번호를 넣으면 아이콘 이미지를 가져올 수 있습니다
+  // weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${weather.icon}@2x.png" style="margin: auto" alt="icon" />`;
 };
 
 // module.exports = handleSuccess;
