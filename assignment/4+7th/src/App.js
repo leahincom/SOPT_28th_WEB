4179dccc04340063c462559b6a5a240a27a72228
JungHyun <leahincom@gmail.com>
2021-06-22 16:38:24 +0900

    feat : Calendar 스타일 적용 및 컴포넌트 정의, App.js 수정

--- assignment/4+7th/src/App.js
+++ assignment/4+7th/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import MainHeader from "./components/common/MainHeader";
 import Calendar from "./components/common/Calendar";
@@ -8,14 +8,29 @@ import Main from "./pages/Main";
 import Diary from "./pages/Diary";
 // Card와 NewCard를 제외하고 모두 불러와주세요!
 
+const getCurrDate = () => {
+  const now = new Date();
+  const currYear = now.getFullYear();
+  const currMonth = now.getMonth();
+  return { year: currYear, month: currMonth };
+};
+
 // BrowserRouter 내부에 모두 넣어줍시다
 // Switch를 사용하는 이유 -> URL이 path에 없으면 Page Not Found를 불러오기 위해서
 function App() {
+  const [year, setYear] = useState(getCurrDate().year);
+  const [month, setMonth] = useState(getCurrDate().month);
+
   return (
     <>
       <BrowserRouter>
         <MainHeader />
-        <Calendar />
+        <Calendar
+          currYear={year}
+          setCurrYear={setYear}
+          currMonth={month}
+          setCurrMonth={setMonth}
+        />
         <Title />
         <Switch>
           <Route exact path="/" component={Main} />
---
+++ assignment/4+7th/src/components/common/Calendar.js
@@ -0,0 +1,114 @@
+// 버튼 아이콘 4가지를 모두 가져옵니다
+import LeftOff from "../../assets/LeftOff.svg";
+import LeftOn from "../../assets/LeftOn.svg";
+import RightOff from "../../assets/RightOff.svg";
+import RightOn from "../../assets/RightOn.svg";
+
+import React, { useRef } from "react";
+import styled from "styled-components";
+
+const Calendar = ({ currYear, setCurrYear, currMonth, setCurrMonth }) => {
+  const monthList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+  // useRef를 사용하여 DOM 요소를 가져올 수 있는 변수를 선언합니다
+  const leftButton = useRef();
+  const rightButton = useRef();
+
+  return (
+    <CalendarWrap>
+      <div className="calendar">
+        <div className="calendar__year">
+          <img
+            src={LeftOff}
+            alt=""
+            onClick={() => setCurrYear(currYear - 1)}
+            onMouseEnter={() => (leftButton.current.src = LeftOn)}
+            onMouseLeave={() => (leftButton.current.src = LeftOff)}
+            ref={leftButton}
+            className="calendar__year--left"
+          />
+          <div className="calendar__year--number">{currYear}년</div>
+          <img
+            src={RightOff}
+            alt=""
+            onClick={() => setCurrYear(currYear + 1)}
+            onMouseEnter={() => (rightButton.current.src = RightOn)}
+            onMouseLeave={() => (rightButton.current.src = RightOff)}
+            ref={rightButton}
+            className="calendar__year--right"
+          />
+        </div>
+        <div className="calendar__month">
+          {monthList.map((month) => {
+            return (
+              <div
+                key={month}
+                onClick={() => setCurrMonth(month)}
+                style={
+                  month === currMonth
+                    ? { fontSize: "22px", fontWeight: "bold" }
+                    : {}
+                }
+                className="calendar__month--button"
+              >
+                {month}월
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </CalendarWrap>
+  );
+};
+
+export default Calendar;
+
+const CalendarWrap = styled.div`
+  .calendar {
+    width: 1200px;
+    height: 118px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    &__year {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: flex-end;
+      height: 61px;
+
+      &--left:hover,
+      &--right:hover {
+        cursor: pointer;
+      }
+
+      &--number {
+        font-size: 36px;
+        font-weight: bold;
+        margin: 0 25px;
+        line-height: 1;
+      }
+    }
+
+    &__month {
+      height: 57px;
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      width: 1025px;
+
+      &--button {
+        font-size: 18px;
+        width: 52px;
+        &:hover {
+          font-size: 22px;
+          font-weight: bold;
+          cursor: pointer;
+        }
+      }
+    }
+  }
+`;
