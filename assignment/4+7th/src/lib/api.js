6b9f517819f6f1820544d69ba5ac902f82f00298
JungHyun <leahincom@gmail.com>
2021-06-22 20:43:14 +0900

    feat : api post 작성 및 instance 생성

--- assignment/4+7th/src/lib/api.js
+++ assignment/4+7th/src/lib/api.js
@@ -1,12 +1,30 @@
 import axios from "axios";
 
+const instance = axios.create({
+  baseURL: "http://localhost:3001",
+  timeout: 1000
+});
+
 export const getCardData = async () => {
   try {
-    const rawData = await axios.get("http://localhost:3001/posts");
+    const rawData = await instance.get("/posts");
     console.log("[SUCCESS] GET card data");
     return rawData.data.data;
   } catch (e) {
     console.log("[FAIL] GET card data");
+    return null;
+  }
+};
+
+export const createCardData = async (userData) => {
+  try {
+    const rawData = await instance.post("/posts", {
+      data: userData
+    });
+    console.log("[SUCCESS] POST card data");
+    return rawData.data.data;
+  } catch (e) {
+    console.log("[FAIL] POST card data");
     return null;
   }
 };
