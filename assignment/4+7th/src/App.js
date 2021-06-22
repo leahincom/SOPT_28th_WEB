83c67032a757a35f5cd91de4f4ef6ff4cd3d1ada
JungHyun <leahincom@gmail.com>
2021-06-22 15:39:01 +0900

    feat : App.js 컴포넌트 구조 정하기

--- assignment/4+7th/src/App.js
+++ assignment/4+7th/src/App.js
@@ -1,25 +1,29 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import MainHeader from "./components/common/MainHeader";
+import Calendar from "./components/common/Calendar";
+import Title from "./components/common/Title";
+import Footer from "./components/common/Footer";
+import Main from "./pages/Main";
+import Diary from "./pages/Diary";
+// Card와 NewCard를 제외하고 모두 불러와주세요!
 
+// BrowserRouter 내부에 모두 넣어줍시다
+// Switch를 사용하는 이유 -> URL이 path에 없으면 Page Not Found를 불러오기 위해서
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <MainHeader />
+        <Calendar />
+        <Title />
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/diary/:id" component={Diary} />
+          <Route component={() => <div>Page Not Found</div>} />
+        </Switch>
+      </BrowserRouter>
+      <Footer />
+    </>
   );
 }
-
-export default App;
